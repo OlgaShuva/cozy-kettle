babb59e4138de5868a21310776d0d51536fd9cdd
OlgaShuva <tatyana20011972@gmail.com>
2025-05-30 03:48:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
-import os
 
-# Инициализация приложения
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = os.urandom(24)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ваш-секретный-ключ'
 
-# Инициализация расширений
+# Инициализация БД
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-# Модель User должна быть определена ПОСЛЕ инициализации db
+# Модель пользователя
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
 
+# Загрузчик пользователя
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
