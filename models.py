eb1becb4da0b948144f877912a6d9580513e387f
OlgaShuva <tatyana20011972@gmail.com>
2025-05-30 03:28:59 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,9 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import db
 from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
     phone = db.Column(db.String(20))
